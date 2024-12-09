11b18f473677b8bd4cc5688891f946ec4f5427e6
Luffytaro05 <dxtrzpc26@gmail.com>
2024-12-09 23:49:54 +0800

    name changes and model was added

---
+++ EmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem
+{
+    internal class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public string ContactInfo { get; set; }
+        public string Status { get; set; } // Active, On-Leave, Terminated
+    }
+}
